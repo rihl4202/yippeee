44ba9bf0a89db328bb126afe232ef0830c7e97de
rihl4202 <88212189+rihl4202@users.noreply.github.com>
2023-06-25 11:54:37 -0400

    Add files via upload

---
+++ Position.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent("move-position", {
+    schema: {
+      moveY: { type: "number", default: 0 },
+    },
+    tick: function () {
+  
+      window.addEventListener("click", (e) => {   
+  
+        this.data.moveY = this.data.moveY + 0.5;
+  
+      });
+      var pos = this.el.getAttribute("position");
+      pos.y = pos.y + this.data.moveY;
+      this.el.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
+    }
+  });
